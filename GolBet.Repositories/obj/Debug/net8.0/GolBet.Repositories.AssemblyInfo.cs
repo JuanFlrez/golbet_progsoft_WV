d6a3c5f3b93fcb69f95dd9a3eab777a046ad78fd
catga <catgamer1012@gmail.com>
2026-09-01 11:20:42 -0500

    Add generic repository, match repository and database seeder (Module 3)

--- GolBet.Repositories/obj/Debug/net8.0/GolBet.Repositories.AssemblyInfo.cs
+++ GolBet.Repositories/obj/Debug/net8.0/GolBet.Repositories.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GolBet.Repositories")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0ce2b272519e08b14fa938b66099fe97d656291b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+147798ce0190e1902f3f1c4c79a264084840ee1a")]
 [assembly: System.Reflection.AssemblyProductAttribute("GolBet.Repositories")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GolBet.Repositories")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
